84edab42a3e513703851ac9ac7ffe79b4b71b66b
Manav Misra <manavm@visionwebsoft.com>
2022-06-01 17:16:40 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // TODO: Update this to reflect wherever you need to load Tailwind CSS from
+  content: ["./src/index.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("prettier-plugin-tailwindcss")],
+};
